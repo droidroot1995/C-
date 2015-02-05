d0211bfb46a5f392d26b08d6f5e2cb52afeadda6
droidroot <goryakin@phystech.edu>
2015-02-05 20:38:14 +0300

    My C# code
    My C# code

---
+++ Системы счисления/Системы счисления/Common/Перевод чисел.xaml.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Шаблон элемента пустой страницы задокументирован по адресу http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Системы_счисления.Common
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class Перевод_чисел : Page
+    {
+        public Перевод_чисел()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Вызывается перед отображением этой страницы во фрейме.
+        /// </summary>
+        /// <param name="e">Данные о событиях, описывающие, каким образом была достигнута эта страница.  Свойство Parameter
+        /// обычно используется для настройки страницы.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Системы_счисления.MainPage));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int x, y, i, z, c, m, m1;
+            string s, s1, s2;
+            m1 = 0;
+            s1 = null;
+            s2 = null;
+            int[] chisl = new int[256];
+            x = Convert.ToInt32(textBox1.Text);
+            y = Convert.ToInt32(textBox2.Text);
+            i = 0;
+            if (x == 0 | y == 0 | x == 1 | y == 1 | x > 16 | y > 16)
+            {
+                label5.Text = "Ошибка ввода системы счисления!";
+            }
+            else if (x == 10 & y <= 10)
+            {
+                s = textBox3.Text;
+                if (s != "Число")
+                {
+                    z = Convert.ToInt32(textBox3.Text);
+                    while (z > 0)
+                    {
+                        chisl[i] = z % y;
+                        z = z / y;
+                        i = i + 1;
+                    }
+                    for (c = i - 1; c > -1; c--)
+                    {
+                        s1 = s1 + chisl[c];
+                    }
+                    label5.Text = s1;
+                }
+                else label5.Text = "Ошибка ввода!";
+            }
+            else
+            {
+                s = textBox3.Text;
+                if(s!="Число")
+                {
+                    m = s.Length;
+                    for (c = 0; c < m - 1; c++)
+                    {
+                        if (s[c] == '1')
+                        {
+                            m1 = (m1 + 1) * x;
+                        }
+                        else if (s[c] == '2')
+                        {
+                            m1 = (m1 + 2) * x;
+                        }
+                        else if (s[c] == '3')
+                        {
+                            m1 = (m1 + 3) * x;
+                        }
+                        else if (s[c] == '4')
+                        {
+                            m1 = (m1 + 4) * x;
+                        }
+                        else if (s[c] == '5')
+                        {
+                            m1 = (m1 + 5) * x;
+                        }
+                        else if (s[c] == '6')
+                        {
+                            m1 = (m1 + 6) * x;
+                        }
+                        else if (s[c] == '7')
+                        {
+                            m1 = (m1 + 7) * x;
+                        }
+                        else if (s[c] == '8')
+                        {
+                            m1 = (m1 + 8) * x;
+                        }
+                        else if (s[c] == '9')
+                        {
+                            m1 = (m1 + 9) * x;
+                        }
+                        else if (s[c] == 'A')
+                        {
+                            m1 = (m1 + 10) * x;
+                        }
+                        else if (s[c] == 'B')
+                        {
+                            m1 = (m1 + 11) * x;
+                        }
+                        else if (s[c] == 'C')
+                        {
+                            m1 = (m1 + 12) * x;
+                        }
+                        else if (s[c] == 'D')
+                        {
+                            m1 = (m1 + 13) * x;
+                        }
+                        else if (s[c] == 'E')
+                        {
+                            m1 = (m1 + 14) * x;
+                        }
+                        else if (s[c] == 'F')
+                        {
+                            m1 = (m1 + 15) * x;
+                        }
+                    }
+                    if (s[c] == '1')
+                    {
+                        m1 = (m1 + 1);
+                    }
+                    else if (s[c] == '2')
+                    {
+                        m1 = (m1 + 2);
+                    }
+                    else if (s[c] == '3')
+                    {
+                        m1 = (m1 + 3);
+                    }
+                    else if (s[c] == '4')
+                    {
+                        m1 = (m1 + 4);
+                    }
+                    else if (s[c] == '5')
+                    {
+                        m1 = (m1 + 5);
+                    }
+                    else if (s[c] == '6')
+                    {
+                        m1 = (m1 + 6);
+                    }
+                    else if (s[c] == '7')
+                    {
+                        m1 = (m1 + 7);
+                    }
+                    else if (s[c] == '8')
+                    {
+                        m1 = (m1 + 8);
+                    }
+                    else if (s[c] == '9')
+                    {
+                        m1 = (m1 + 9);
+                    }
+                    else if (s[c] == 'A')
+                    {
+                        m1 = (m1 + 10);
+                    }
+                    else if (s[c] == 'B')
+                    {
+                        m1 = (m1 + 11);
+                    }
+                    else if (s[c] == 'C')
+                    {
+                        m1 = (m1 + 12);
+                    }
+                    else if (s[c] == 'D')
+                    {
+                        m1 = (m1 + 13);
+                    }
+                    else if (s[c] == 'E')
+                    {
+                        m1 = (m1 + 14);
+                    }
+                    else if (s[c] == 'F')
+                    {
+                        m1 = (m1 + 15);
+                    }
+                    while (m1 > 0)
+                    {
+                        if (m1 % y == 10)
+                        {
+                            s1 = s1 + 'A';
+                        }
+                        else if (m1 % y == 11)
+                        {
+                            s1 = s1 + 'B';
+                        }
+                        else if (m1 % y == 12)
+                        {
+                            s1 = s1 + 'C';
+                        }
+                        else if (m1 % y == 13)
+                        {
+                            s1 = s1 + 'D';
+                        }
+                        else if (m1 % y == 14)
+                        {
+                            s1 = s1 + 'E';
+                        }
+                        else if (m1 % y == 15)
+                        {
+                            s1 = s1 + 'F';
+                        }
+                        else s1 = s1 + (m1 % y);
+                        m1 = m1 / y;
+                        i = i + 1;
+                    }
+                    for (c = i - 1; c > -1; c--)
+                    {
+                        s2 = s2 + s1[c];
+                    }
+                    label5.Text = s2;
+            }
+            else label5.Text = "Ошибка ввода!";
+            }
+        }
+    }
+}
---
+++ камень ножницы бумага/камень ножницы бумага/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace камень_ножницы_бумага
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        const int Max = 10;
+        int Object, Comp, Ty, Vvod, Button;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text += "    Version:  " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            Random rand = new Random();
+            Object = rand.Next(0, 2);
+            Ty = 0;
+            Comp = 0;
+            label3.Text = Convert.ToString(Ty);
+            label4.Text = Convert.ToString(Comp);
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Vvod = 0;
+            if (Vvod == Object)
+            {
+                Ty = Ty + 0;
+                Comp = Comp + 0;
+                label1.Text = "Камень";
+                label2.Text = "Камень";
+                pictureBox5.Image = pictureBox2.Image;
+                pictureBox6.Image = pictureBox2.Image;
+            }
+            if (Object == 1)
+            {
+                Comp++;
+                label1.Text = "Камень";
+                label2.Text = "Бумага";
+                pictureBox5.Image = pictureBox2.Image;
+                pictureBox6.Image = pictureBox3.Image;
+            }
+            if (Object == 2)
+            {
+                Ty++;
+                label1.Text = "Камень";
+                label2.Text = "Ножницы";
+                pictureBox5.Image = pictureBox2.Image;
+                pictureBox6.Image = pictureBox4.Image;
+            }
+            if (Ty == Max)
+            {
+                label5.Text = "Вы выиграли!";
+                DialogResult Button = MessageBox.Show("Новая Игра?", "Новая Игра?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (Button == DialogResult.Yes)
+                {
+                  Random rnd = new Random();
+            Object = rnd.Next(0, 2);
+            Ty = 0;
+            Comp = 0;
+            label3.Text = Convert.ToString(Ty);
+            label4.Text = Convert.ToString(Comp);
+                }
+                else
+                    Close();
+            }
+            if (Comp == Max)
+            {
+                label5.Text = "Вы проиграли!";
+                DialogResult Button = MessageBox.Show("Новая Игра?", "Новая Игра?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (Button == DialogResult.Yes)
+                {
+                    Random rnd = new Random();
+                    Object = rnd.Next(0, 2);
+                    Ty = 0;
+                    Comp = 0;
+                    label3.Text = Convert.ToString(Ty);
+                    label4.Text = Convert.ToString(Comp);
+                }
+                else
+                    Close();
+            }
+            label3.Text = Convert.ToString(Ty);
+            label4.Text = Convert.ToString(Comp);
+            Random rand = new Random();
+            Object = rand.Next(0, 2);
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            Vvod = 1;
+            if (Vvod == Object)
+            {
+                Ty = Ty + 0;
+                Comp = Comp + 0;
+                label1.Text = "Бумага";
+                label2.Text = "Бумага";
+                pictureBox5.Image = pictureBox3.Image;
+                pictureBox6.Image = pictureBox3.Image;
+            }
+            if (Object == 0)
+            {
+                Ty++;
+                label1.Text = "Бумага";
+                label2.Text = "Камень";
+                pictureBox5.Image = pictureBox3.Image;
+                pictureBox6.Image = pictureBox2.Image;
+            }
+            if (Object == 2)
+            {
+                Comp++;
+                label1.Text = "Бумага";
+                label2.Text = "Ножницы";
+                pictureBox5.Image = pictureBox3.Image;
+                pictureBox6.Image = pictureBox4.Image;
+            }
+            if (Ty == Max)
+            {
+                label5.Text = "Вы выиграли!";
+                DialogResult Button = MessageBox.Show("Новая Игра?", "Новая Игра?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (Button == DialogResult.Yes)
+                {
+                    Random rnd = new Random();
+                    Object = rnd.Next(0, 2);
+                    Ty = 0;
+                    Comp = 0;
+                    label3.Text = Convert.ToString(Ty);
+                    label4.Text = Convert.ToString(Comp);
+                }
+                else
+                    Close();
+            }
+            if (Comp == Max)
+            {
+                label5.Text = "Вы проиграли!";
+                DialogResult Button = MessageBox.Show("Новая Игра?", "Новая Игра?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (Button == DialogResult.Yes)
+                {
+                    Random rnd = new Random();
+                    Object = rnd.Next(0, 2);
+                    Ty = 0;
+                    Comp = 0;
+                    label3.Text = Convert.ToString(Ty);
+                    label4.Text = Convert.ToString(Comp);
+                }
+                else
+                    Close();
+            }
+            label3.Text = Convert.ToString(Ty);
+            label4.Text = Convert.ToString(Comp);
+            Random rand = new Random();
+            Object = rand.Next(0, 2);
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            Vvod = 2;
+            if (Vvod == Object)
+            {
+                Ty = Ty + 0;
+                Comp = Comp + 0;
+                label1.Text = "Ножницы";
+                label2.Text = "Ножницы";
+                pictureBox5.Image = pictureBox4.Image;
+                pictureBox6.Image = pictureBox4.Image;
+            }
+            if (Object == 0)
+            {
+                Comp++;
+                label1.Text = "Ножницы";
+                label2.Text = "Камень";
+                pictureBox5.Image = pictureBox4.Image;
+                pictureBox6.Image = pictureBox2.Image;
+            }
+            if (Object == 1)
+            {
+                Ty++;
+                label1.Text = "Ножницы";
+                label2.Text = "Бумага";
+                pictureBox5.Image = pictureBox4.Image;
+                pictureBox6.Image = pictureBox3.Image;
+            }
+            if (Ty == Max)
+            {
+                label5.Text = "Вы выиграли!";
+                DialogResult Button = MessageBox.Show("Новая Игра?", "Новая Игра?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (Button == DialogResult.Yes)
+                {
+                    Random rnd = new Random();
+                    Object = rnd.Next(0, 2);
+                    Ty = 0;
+                    Comp = 0;
+                    label3.Text = Convert.ToString(Ty);
+                    label4.Text = Convert.ToString(Comp);
+                }
+                else
+                    Close();
+            }
+            if (Comp == Max)
+            {
+                label5.Text = "Вы проиграли!";
+                DialogResult Button = MessageBox.Show("Новая Игра?", "Новая Игра?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (Button == DialogResult.Yes)
+                {
+                    Random rnd = new Random();
+                    Object = rnd.Next(0, 2);
+                    Ty = 0;
+                    Comp = 0;
+                    label3.Text = Convert.ToString(Ty);
+                    label4.Text = Convert.ToString(Comp);
+                }
+                else
+                    Close();
+            }
+            label3.Text = Convert.ToString(Ty);
+            label4.Text = Convert.ToString(Comp);
+            Random rand = new Random();
+            Object = rand.Next(0, 2);
+        }
+    }
+}
